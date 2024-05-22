147ffa1cae1de9b8a5db62564028e65ee72c5396
Alex Bryan <abryan9@uwyo.edu>
2024-05-22 15:37:20 -0600

    EOD 5/22

--- checkerboard.js
+++ checkerboard.js
@@ -6,12 +6,30 @@ This file contains all code relating to checkboard and will manipulate the DOM i
 function setChecker() {
     console.log('Switching to checkerboard')
     let progGuts = document.querySelector('.program_board .program_guts')
-    let p = document.createElement('p')
-    p.innerHTML = 'hello world - checkerboard'
-    p.classList.add('program_guts')
-    progGuts.replaceWith(p)
+
+    let main_container = document.createElement('div')
+    main_container.classList.add('cb_container')
+
+    let header = document.createElement('p')
+    header.classList.add('cb_header')
+    header.innerHTML = 'hello world - checkerboard'
+
+    let list_container = document.createElement('div')
+    list_container.innerHTML = 'another child'
+
+    let room_list = document.createElement('span')
+    room_list.style='float: left'
+    room_list.classList.add('cb_contents')
+    room_list.innerHTML = 'Test 1 <br>A longer statement'
+
+    let available_list = document.createElement('span')
+    available_list.style='float: left'
+    available_list.classList.add('cb_contents')
+    available_list.innerHTML = 'Test 2 <br>A longer statement'
+
+    list_container.append(room_list, available_list)
+
+    main_container.append(header, list_container)
+    progGuts.replaceWith(main_container)
     return
   }
-  
-
-  
