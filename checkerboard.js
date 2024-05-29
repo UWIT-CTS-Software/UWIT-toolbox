0b718d3807db66e2c3cd251ae60f141a99a2574f
Alex Bryan <abryan9@uwyo.edu>
2024-05-29 11:51:48 -0600

    DOMgit add -Agit add -A

--- checkerboard.js
+++ checkerboard.js
@@ -3,33 +3,39 @@ checkboard.js
 
 This file contains all code relating to checkboard and will manipulate the DOM in index.html accordingly
 */
+
+
 function setChecker() {
-    console.log('Switching to checkerboard')
-    let progGuts = document.querySelector('.program_board .program_guts')
+    console.log('Switching to checkerboard');
+    let progGuts = document.querySelector('.program_board .program_guts');
 
-    let main_container = document.createElement('div')
-    main_container.classList.add('cb_container')
+    let main_container = document.createElement('div');
+    main_container.classList.add('cb_container');
 
-    let header = document.createElement('p')
-    header.classList.add('cb_header')
-    header.innerHTML = 'hello world - checkerboard'
+    let header = document.createElement('div');
+    header.classList.add('cb_header');
+    header.innerHTML = '<p>hello world - checkerboard</p>';
 
-    let list_container = document.createElement('div')
-    list_container.innerHTML = 'another child'
+    let list_container = document.createElement('div');
+    list_container.innerHTML = 'another child<br>';
 
-    let room_list = document.createElement('span')
-    room_list.style='float: left'
-    room_list.classList.add('cb_contents')
-    room_list.innerHTML = 'Test 1 <br>A longer statement'
+    let room_list, available_list, schedule_list, checked_list, needs_checked_list, comments_list;
+    let container_list = [room_list, available_list, schedule_list, checked_list, needs_checked_list, comments_list];
 
-    let available_list = document.createElement('span')
-    available_list.style='float: left'
-    available_list.classList.add('cb_contents')
-    available_list.innerHTML = 'Test 2 <br>A longer statement'
+    for (list in container_list) {
+      list = document.createElement('div');
+      list.classList.add('.cb_contents')
+      let inner_string = '';
+      for (i=0; i<10; i++) {
+        inner_string += 'test ';
+      }
+      list.innerHTML = inner_string + '!';
+      list_container.append(list);
+    }
 
-    list_container.append(room_list, available_list)
-
-    main_container.append(header, list_container)
-    progGuts.replaceWith(main_container)
-    return
+    main_container.append(header, list_container);
+    main_container.classList.add('program_guts');
+    console.log(main_container)
+    progGuts.replaceWith(main_container);
+    return;
   }
